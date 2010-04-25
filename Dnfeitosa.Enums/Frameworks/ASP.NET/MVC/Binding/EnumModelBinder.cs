1c4a9d1f5383e77207aebf7e9db7e0d7dfefe9be
Diego Feitosa <dnfeitosa@gmail.com>
2010-04-25 01:12:35 -0300

    added ASP.NET MVC model binder

---
+++ Dnfeitosa.Enums/Frameworks/ASP.NET/MVC/Binding/EnumModelBinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace Dnfeitosa.Enums.Frameworks.ASP.NET.MVC.Binding
+{
+    public class EnumModelBinder : IModelBinder
+    {
+        private readonly IModelBinder _defaultBinder;
+
+        public EnumModelBinder(ModelBinderDictionary binders)
+        {
+            if (binders == null)
+                throw new ArgumentNullException("binders");
+            _defaultBinder = binders.DefaultBinder;
+            binders.DefaultBinder = this;
+        }
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            if (!IsEnumType(bindingContext.ModelType))
+                return _defaultBinder.BindModel(controllerContext, bindingContext);
+
+            var enumName = controllerContext.HttpContext.Request[bindingContext.ModelName];
+            if (enumName == null || enumName.Trim() == "")
+                return null;
+
+            var @enum = bindingContext.ModelType
+                .GetFields(BindingFlags.Static | BindingFlags.Public)
+                .Where(f => f.Name == enumName)
+                .FirstOrDefault();
+
+            return @enum == null ? null : @enum.GetValue(null);
+        }
+
+        private bool IsEnumType(Type type)
+        {
+            return type
+                .GetInterfaces()
+                .Any(iface => iface == typeof(IEnum));
+        }
+    }
+}
