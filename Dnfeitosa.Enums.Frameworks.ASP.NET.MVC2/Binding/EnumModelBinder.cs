de2895629c6c8db8cd8b6ad28f802cf355f0663b
Diego Feitosa <dnfeitosa@gmail.com>
2010-11-23 03:49:14 -0200

    added model binder for Asp.NET MVC 2

---
+++ Dnfeitosa.Enums.Frameworks.ASP.NET.MVC2/Binding/EnumModelBinder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using Dnfeitosa.Enums.Helpers;
+
+namespace Dnfeitosa.Enums.Frameworks.ASP.NET.MVC2.Binding
+{
+    public class EnumModelBinder : IModelBinder
+    {
+        private readonly IModelBinder _defaultBinder;
+
+        public EnumModelBinder(ModelBinderDictionary binders)
+        {
+            if (binders == null)
+                throw new ArgumentNullException("binders");
+            _defaultBinder = binders.DefaultBinder;
+            binders.DefaultBinder = this;
+        }
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var modelType = bindingContext.ModelType;
+            if (!modelType.IsAnEnumType())
+                return _defaultBinder.BindModel(controllerContext, bindingContext);
+
+            var enumName = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).AttemptedValue;
+            if (enumName == null || enumName.Trim() == "")
+                return null;
+
+            var @enum = modelType.GetFieldNamed(enumName);
+            return @enum == null
+                ? null
+                : @enum.GetValue(null);
+        }
+
+        public static void Setup()
+        {
+            new EnumModelBinder(ModelBinders.Binders);
+        }
+    }
+}
