3e984b5e902c3daac7f8fd29d72454d5ed8eb043
Diego Feitosa <dnfeitosa@gmail.com>
2010-11-28 14:30:23 -0200

    added custom usertype for NHibernate

---
+++ Dnfeitosa.Enums.Frameworks.NHibernate2.Test/Types/EnumTypeTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using Dnfeitosa.Enums.Frameworks.NHibernate2.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Dnfeitosa.Enums.Frameworks.NHibernate2.Test.Types
+{
+    [TestClass]
+    public class EnumTypeTest
+    {
+        private EnumType<Tests.Fixtures.Language> _enumType;
+        private MockFactory _mockFactory;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockFactory = new MockFactory(MockBehavior.Loose);
+            _enumType = new EnumType<Tests.Fixtures.Language>();
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenTheValueRetrievedFromDatabaseIsNotAnEnum()
+        {
+            var dataReader = GivenADataReaderThatReturns("someUnexpectedValue");
+
+            var @enum = _enumType.NullSafeGet(dataReader.Object, new [] { "column_on_reader" }, new object());
+            Assert.IsNull(@enum);
+        }
+
+        [TestMethod]
+        public void ShouldReturnTheEnumCorrespondingToTheStoredName()
+        {
+            var dataReader = GivenADataReaderThatReturns(Tests.Fixtures.Language.Pl.Name);
+
+            var @enum = _enumType.NullSafeGet(dataReader.Object, new[] { "column_on_reader" }, new object());
+            Assert.IsNotNull(@enum);
+
+            Assert.AreEqual(Tests.Fixtures.Language.Pl, @enum);
+        }
+
+        [TestMethod]
+        public void ShouldStoreEnumsNameOnDatabase()
+        {
+            var dbCommand = _mockFactory.Create<IDbCommand>();
+            var parameter = _mockFactory.Create<IDataParameter>(MockBehavior.Loose);
+            dbCommand.SetupGet(cmd => cmd.Parameters[1]).Returns(parameter.Object);
+
+            _enumType.NullSafeSet(dbCommand.Object, Tests.Fixtures.Language.Pl, 1);
+
+            parameter.VerifySet(param => param.Value = Tests.Fixtures.Language.Pl.Name);
+        }
+
+        private Mock<IDataReader> GivenADataReaderThatReturns(string value)
+        {
+            var dataReader = _mockFactory.Create<IDataReader>();
+            dataReader.Setup(reader => reader.GetOrdinal(It.IsAny<string>())).Returns(1);
+            dataReader.Setup(reader => reader.IsDBNull(1)).Returns(false);
+            dataReader.Setup(reader => reader[1]).Returns(value);
+            return dataReader;
+        }
+    }
+}
---
+++ Dnfeitosa.Enums.Frameworks.NHibernate2/Types/EnumType.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using NHibernate;
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+using Dnfeitosa.Enums.Helpers;
+
+namespace Dnfeitosa.Enums.Frameworks.NHibernate2.Types
+{
+    public class EnumType<TEnumType> : IUserType
+        where TEnumType : IEnum
+    {
+        public bool Equals(object x, object y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(object x)
+        {
+            return x.GetHashCode();
+        }
+
+        public object NullSafeGet(IDataReader rs, string[] names, object owner)
+        {
+            var enumName = (string)NHibernateUtil.String.NullSafeGet(rs, names[0]);
+            var enumField = typeof(TEnumType).GetFieldNamed(enumName);
+            return enumField == null
+                ? null
+                : enumField.GetValue(null);
+        }
+
+        public void NullSafeSet(IDbCommand cmd, object value, int index)
+        {
+            if (value == null)
+            {
+                NHibernateUtil.String.NullSafeSet(cmd, null, index);
+                return;
+            }
+
+            var @enum = (IEnum)value;
+            NHibernateUtil.String.NullSafeSet(cmd, @enum.Name, index);
+        }
+
+        public object DeepCopy(object value)
+        {
+            return value;
+        }
+
+        public object Replace(object original, object target, object owner)
+        {
+            return original;
+        }
+
+        public object Assemble(object cached, object owner)
+        {
+            return cached;
+        }
+
+        public object Disassemble(object value)
+        {
+            return value;
+        }
+
+        public SqlType[] SqlTypes
+        {
+            get
+            {
+                return new[] { new SqlType(DbType.String) };
+            }
+        }
+
+        public Type ReturnedType
+        {
+            get { return typeof(TEnumType); }
+        }
+
+        public bool IsMutable
+        {
+            get { return false; }
+        }
+
+    }
+}
