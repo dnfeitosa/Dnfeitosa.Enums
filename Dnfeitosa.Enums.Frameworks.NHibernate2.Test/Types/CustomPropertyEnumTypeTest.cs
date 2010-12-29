a85008cdb29cafddb61b733db4a2bf76d829d3e8
Diego Feitosa <dnfeitosa@gmail.com>
2010-12-29 02:28:23 -0200

    NHibernate mapping using custom properties (not yet finished)

---
+++ Dnfeitosa.Enums.Frameworks.NHibernate2.Test/Types/CustomPropertyEnumTypeTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Dnfeitosa.Enums.Frameworks.NHibernate2.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dnfeitosa.Enums.Frameworks.NHibernate2.Test.Types
+{
+    [TestClass]
+    public class CustomPropertyEnumTypeTest
+    {
+
+        [TestMethod]
+        public void Foo()
+        {
+            new LanguageType();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowExceptionWhenExpressionIsNotAMemberAccess()
+        {
+            new CustomPropertyEnumTypeFixture();
+        }
+    }
+
+    public class CustomPropertyEnumTypeFixture : CustomPropertyEnumType<Tests.Fixtures.Language>
+    {
+        public CustomPropertyEnumTypeFixture()
+            : base(exp => exp.ToString())
+        {
+        }
+    }
+
+    public class LanguageType : CustomPropertyEnumType<Tests.Fixtures.Language>
+    {
+        public LanguageType()
+            : base(e => e.IetfTag)
+        {
+            
+        }
+    }
+}
---
+++ Dnfeitosa.Enums.Frameworks.NHibernate2/Types/CustomPropertyEnumType.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using NHibernate;
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+
+namespace Dnfeitosa.Enums.Frameworks.NHibernate2.Types
+{
+    public abstract class CustomPropertyEnumType<TEnumType> : IUserType
+             where TEnumType : IEnum
+    {
+        public PropertyInfo EnumProperty { get; private set; }
+
+        protected CustomPropertyEnumType(Expression<Func<TEnumType, object>> expression)
+        {
+            if (expression.Body.NodeType != ExpressionType.MemberAccess)
+            {
+                throw new ArgumentException("Not a member access", "expression");
+            }
+            var memberExpression = expression.Body as MemberExpression;
+            EnumProperty = (PropertyInfo)memberExpression.Member;
+        }
+
+
+        public bool Equals(object x, object y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(object x)
+        {
+            return x.GetHashCode();
+        }
+
+        public object NullSafeGet(IDataReader rs, string[] names, object owner)
+        {
+            var enumName = (string)NHibernateUtil.String.NullSafeGet(rs, names[0]);
+            var enumProperty = typeof (TEnumType).GetProperties().FirstOrDefault(p => p == EnumProperty);
+            return enumProperty == null
+                ? null
+                : enumProperty.GetV
+        }
+
+        public void NullSafeSet(IDbCommand cmd, object value, int index)
+        {
+            if (value == null)
+            {
+                NHibernateUtil.String.NullSafeSet(cmd, null, index);
+                return;
+            }
+
+            var @enum = (IEnum)value;
+            NHibernateUtil.String.NullSafeSet(cmd, @enum.Name, index);
+        }
+
+        public object DeepCopy(object value)
+        {
+            return value;
+        }
+
+        public object Replace(object original, object target, object owner)
+        {
+            return original;
+        }
+
+        public object Assemble(object cached, object owner)
+        {
+            return cached;
+        }
+
+        public object Disassemble(object value)
+        {
+            return value;
+        }
+
+        public SqlType[] SqlTypes
+        {
+            get
+            {
+                return new[] { new SqlType(DbType.String) };
+            }
+        }
+
+        public Type ReturnedType
+        {
+            get { return typeof(TEnumType); }
+        }
+
+        public bool IsMutable
+        {
+            get { return false; }
+        }
+    }
+}
